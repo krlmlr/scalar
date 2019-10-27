d6ceb8616b8cfda58843682eaaa2e4f64610dfd9
Kirill Müller <krlmlr+r@mailbox.org>
2019-10-27 18:11:34 +0100

    Can concatenate

--- tests/testthat/test-scalar.R
+++ tests/testthat/test-scalar.R
@@ -1,4 +1,4 @@
-test_that("can create and concatenate", {
+test_that("can create and coerce", {
   expect_equal(vec_c(scalar(1), .ptype = numeric()), 1)
   expect_error(vec_c(scalar(1), .ptype = character()))
 })
@@ -7,4 +7,10 @@ test_that("can find common type", {
   expect_equal(vec_ptype2(scalar(1), scalar(2)), numeric())
   expect_equal(vec_ptype2(scalar(1), 2:3), numeric())
   expect_equal(vec_ptype2(0:1, scalar(2)), numeric())
+})
+
+test_that("can concatenate", {
+  expect_equal(vec_c(scalar(1), scalar(2)), 1:2)
+  expect_equal(vec_c(scalar(1), 2:3), 1:3)
+  expect_equal(vec_c(0:1, scalar(2)), 0:2)
 })
